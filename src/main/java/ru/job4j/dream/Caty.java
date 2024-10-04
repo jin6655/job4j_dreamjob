19dc7dd666fad9d86477adab72be1a8d7cadbcb7
vasily <vasilypoiser@@yandex.ru>
2024-10-04 17:46:55 +0300

    4.1. JSON формат.

--- src/main/java/ru/job4j/dream/Caty.java
+++ src/main/java/ru/job4j/dream/Caty.java
@@ -1,12 +1,39 @@
 package ru.job4j.dream;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.Arrays;
+
 public class Caty {
 
-    public static void main(String[] args) {
-        System.out.println("kandinsky-download-1727127980318.png");
-        String str = "kandinsky-download-1727127980318.png";
-        System.out.println(str.split("\\.")[1]);
+    private int x;
+    private String str;
+    private String[] massStr;
 
+    public Caty() {
+    }
+
+    public Caty(int x, String str, String[] massStr) {
+        this.x = x;
+        this.str = str;
+        this.massStr = massStr;
+    }
+
+    @Override
+    public String toString() {
+        return "Caty{" +
+                "x=" + x +
+                ", str='" + str + '\'' +
+                ", massStr=" + Arrays.toString(massStr) +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Caty cat = new Caty(34, "Anny", new String[]{"Old", "Boy", "Girl"});
+        Gson gsonCat = new GsonBuilder().create();
+        Caty cat02 = gsonCat.fromJson(gsonCat.toJson(cat), Caty.class);
+        System.out.println(cat02);
     }
 
 }
--- src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
+++ src/main/java/ru/job4j/dream/servlet/GreetingServlet.java
@@ -1,23 +1,46 @@
 package ru.job4j.dream.servlet;
 
+import com.google.gson.*;
+import ru.job4j.dream.model.Email;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class GreetingServlet extends HttpServlet {
 
+    private  final List<Email> emails = new CopyOnWriteArrayList<>();
+
+    private static final Gson GSON = new GsonBuilder().create();
+
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException, IOException {
-        resp.setContentType("text/plain");
-        resp.setCharacterEncoding("UTF-8");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Email email = GSON.fromJson(req.getReader(), Email.class);
+        emails.add(email);
+        resp.setContentType("application/json; charset=utf-8");
         resp.setHeader("Access-Control-Allow-Origin", "*");
-        String email = req.getParameter("email");
-        PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.println(email);
-        writer.flush();
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(email);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
+    }
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json; charset=utf-8");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        OutputStream output = resp.getOutputStream();
+        String json = GSON.toJson(emails);
+        output.write(json.getBytes(StandardCharsets.UTF_8));
+        output.flush();
+        output.close();
     }
 
 }
